61b06551f44b966fce3b270040a982a0174fc56f
Pin-Xuan She <snakeneedy@gmail.com>
2017-01-04 23:38:01 +0800

    Re-write algorithm

--- js/main.js
+++ js/main.js
@@ -2,9 +2,10 @@ var processes = csv2jsons(data);
 var queue = [];
 var result = [];
 
+//// debug
 for(var i = 0; i < processes.length; i++)
   document.getElementById('debug').innerHTML += JSON.stringify(processes[i]) +'<br/>';
-
+//// debug
 document.getElementById('debug').innerHTML += '<hr/>';
 
 // 對 arrival_time 遞增排序
@@ -16,59 +17,64 @@ processes = processes.sort(function(nxt, pre) {
   return pre['arrival_time'] < nxt['arrival_time'];
 });
 
-for(var i = 0; i < processes.length; i++)
-  document.getElementById('debug').innerHTML += JSON.stringify(processes[i]) +'<br/>';
-
-var start_time = 0,
-    end_time = 0;
-// 對每個程序處理
+var pre_time = 0, cur_time = 0; // check time
 for(var i = 0; i < processes.length; i++) {
-  var peroid = JSON.parse('{}');
-  start_time = end_time;
-  end_time = processes[i]['arrival_time'];
-  //console.log(':'+ start_time +' -> '+ end_time);
-
-  // TODO: 跟序列所有的程序比較
-  // 最優先 && 剩餘時間最少 -> 先做
+  cur_time = processes[i]['arrival_time'];
+  if(queue.length == 0) {
+    // 進來第一個程序
+    pre_time = cur_time;
+  }
+  else {
+    // 檢查 queue 裡面程序是否執行完了？
+    while(queue.length > 0 && pre_time + queue[0]['remain_time'] <= cur_time) {
+      var period = JSON.parse('{}');
+      // 可以被執行完的程序
+//      console.log('[finish] pre_time: '+ pre_time +', cur_time: '+ cur_time +', queue[0]: '+ JSON.stringify(queue[0]));
+      period['id'] = queue[0]['id'];
+      period['start_time'] = pre_time;
+      pre_time += queue[0]['remain_time']; // update
+      period['end_time']   = pre_time;
+      queue.shift(); // pop()
+      console.log('period: '+ JSON.stringify(period));
+      result.push(period);
+//      console.log('      -> pre_time: '+ pre_time +', cur_time: '+ cur_time);
+    }
+    if(queue.length > 0 && pre_time < cur_time) {
+      var period = JSON.parse('{}');
+//      console.log('[ing] pre_time: '+ pre_time +', cur_time: '+ cur_time +', queue[0]: '+ JSON.stringify(queue[0]));
+      period['id'] = queue[0]['id'];
+      period['start_time'] = pre_time;
+      period['end_time']   = cur_time;
+      queue[0]['remain_time'] -= cur_time - pre_time;
+      console.log('period: '+ JSON.stringify(period));
+      result.push(period);
+      pre_time = cur_time; // update
+    }
+  }
   queue.push(processes[i]);
-  queue = queue.sort(function(nxt,pre) {
+  queue.sort(function(nxt, pre) {
     if(pre['priority'] == nxt['priority']) {
       return pre['remain_time'] <= nxt['remain_time'];
     }
     return pre['priority'] < nxt['priority'];
   });
-
-  queue[0]['remain_time'] -= (end_time - start_time);
-  if(queue[0]['remain_time'] <= 0) {
-    end_time += queue[0]['remain_time'];
-    peroid['id'] = queue[0]['id'];
-    queue.shift();
-  }
-  else {
-    peroid['id'] = queue[0]['id'];
-  }
-  peroid['start_time'] = start_time;
-  peroid['end_time']   = end_time;
-  if(peroid['id'] == 2) {
-    console.log('id: '+ peroid['id'] +', s: '+ start_time + ', e: '+ end_time);
-  }
-  result.push(peroid);
 }
 
+pre_time = cur_time; // update
 while(queue.length > 0) {
-  var peroid = JSON.parse('{}');
-  start_time = end_time;
-  end_time += queue[0]['remain_time'];
-
-  peroid['id'] = queue[0]['id'];
-  peroid['start_time'] = start_time;
-  peroid['end_time'] = end_time;
-  queue.shift();
-  result.push(peroid);
+  var period = JSON.parse('{}');
+  // 可以被執行完的程序
+  period['id'] = queue[0]['id'];
+  period['start_time'] = pre_time;
+  pre_time += queue[0]['remain_time']; // update
+  period['end_time']   = pre_time;
+  queue.shift(); // pop()
+  console.log('period: '+ JSON.stringify(period));
+  result.push(period);
+  //      console.log('      -> pre_time: '+ pre_time +', cur_time: '+ cur_time);
 }
 
-document.getElementById('debug').innerHTML += '<hr/>';
-
+//// debug
 //console.log(result);
 for(var i = 0; i < result.length; i++)
   document.getElementById('debug').innerHTML += JSON.stringify(result[i]) +'<br/>';
