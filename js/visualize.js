52da08d714f23eb4c6a13f7dcbebc3ef4fdf84bb
Pin-Xuan She <snakeneedy@gmail.com>
2017-01-05 11:37:27 +0800

    Resize svg visualization

--- js/visualize.js
+++ js/visualize.js
@@ -25,6 +25,7 @@ function visualize(target, ref, data) {
   //console.log(JSON.stringify(data));
   var unitWidth = 5,
     unitHeight = 20;
+  var x, y, width, height;
 
   var d = data.sort(function(nxt, pre) {
     if(pre['id'] == nxt['id']) {
@@ -32,16 +33,16 @@ function visualize(target, ref, data) {
     }
     return pre['id'] < nxt['id'];
   });
-  //var tmp_id = -1, tmp_start_time = -1;
-  //for(var i = 0; i < d.length; i++) {
-  //  if(tmp_id == -1) tmp_id = d[i]['id'];
-  //  if(tmp_end_time == -1) tmp_end_time = d[i]['end_time'];
-  //}
 
+  width = 0;
+  for(var i = 0; i < ref.length; i++) {
+    if(ref[i]['expired_time'] > width)
+      width = ref[i]['expired_time'];
+  }
   var svg = d3.select(target).append('svg')
-    .attr('width', 5000)
+    .attr('width', width * unitWidth)
     .attr('height', ref.length * unitHeight);
-  var x, y, width, height;
+
   for(var i = 0; i < data.length; i++) {
     x = data[i]['start_time'] * unitWidth;
     y = (data[i]['id'] - 1) * unitHeight;
